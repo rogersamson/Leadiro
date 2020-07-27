ee4c306bd82e9164f5b89bf058bd3499e58c1071
Rogelio Samson <rogelio.samson@gmail.com>
2020-07-27 20:26:21 +0800

    Clean Up Imports

--- src/main/java/com/leadiro/starter/service/starter/ConcreteBookService.java
+++ src/main/java/com/leadiro/starter/service/starter/ConcreteBookService.java
@@ -11,9 +11,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.leadiro.starter.exceptions.RecordNotFoundException;
 import com.leadiro.starter.service.BookService;
@@ -28,32 +26,32 @@ public class ConcreteBookService implements BookService {
 	private final String baseUri = "https://collections.museumsvictoria.com.au/api/";
 	
 	@Override
-	public Book[] searchByKeywords(String[] keyword) throws Exception  {
+	public Book[] searchByKeywords(String[] keyword) throws Exception {
 		log.debug("Search books by keyword [{}]", (Object) keyword);
 		Book[] books;
-		
-			ObjectMapper mapper = new ObjectMapper()
-					.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-			RestTemplate restTemplate = new RestTemplate();
-			
-			HttpHeaders headers = new HttpHeaders();
-		
-			UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(baseUri + "search");
-			for(int i=0;i<keyword.length;i++) {
-				builder.queryParam("query", keyword[i]);
-			}			
-			headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-			headers.add("user-agent", userAgent);
-			HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
-			
-			ResponseEntity<String> response = restTemplate.exchange(builder.toUriString(), HttpMethod.GET,entity, String.class);
-			books = mapper.readValue(response.getBody().toString() , Book[].class);
-			if(books.length>0) {
-				log.debug("Search books by keyword [{}] results [{}]",  (Object) keyword, "Record Found");
-			}else {
-				log.debug("Search books by keyword [{}] results [{}]",  (Object) keyword, "Record Not Found");
-				throw new RecordNotFoundException("Record Not Found");
-			}
+
+		ObjectMapper mapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		RestTemplate restTemplate = new RestTemplate();
+
+		HttpHeaders headers = new HttpHeaders();
+
+		UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(baseUri + "search");
+		for (int i = 0; i < keyword.length; i++) {
+			builder.queryParam("query", keyword[i]);
+		}
+		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+		headers.add("user-agent", userAgent);
+		HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+
+		ResponseEntity<String> response = restTemplate.exchange(builder.toUriString(), HttpMethod.GET, entity,
+				String.class);
+		books = mapper.readValue(response.getBody().toString(), Book[].class);
+		if (books.length > 0) {
+			log.debug("Search books by keyword [{}] results [{}]", (Object) keyword, "Record Found");
+		} else {
+			log.debug("Search books by keyword [{}] results [{}]", (Object) keyword, "Record Not Found");
+			throw new RecordNotFoundException("Record Not Found");
+		}
 		
 		return books;
 	}
